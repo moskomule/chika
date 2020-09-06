455b5b93c20d846f738c2acd3afc4f0c6f0979c8
moskomule <hataya@keio.jp>
2020-09-06 15:15:25 +0900

    fix python version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.0.1",
     description="`chika` is a dataclass-based simple and easy config tool",
     author="Ryuichiro Hataya",
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=install_requires,
     packages=find_packages()
 )
